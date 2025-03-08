a31a5804fb506b1c7c86172bfece4cda8cbefaef
mamataranu651 <mamataranu651@gmail.com>
2025-03-08 20:59:41 +0000

    Add 4.php

---
+++ src/4.php
@@ -0,0 +1,5 @@
+<?php
+// Random PHP code generated using random.org API
+$randomCode = file_get_contents('https://www.random.org/integers/?num=1&min=0&max=100&col=1&format=plain');
+echo $randomCode;
+?>
