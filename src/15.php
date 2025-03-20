7f4d2ea78d115d57cbd19e1da71d0d4f7e9a9f9d
mamataranu651 <mamataranu651@gmail.com>
2025-03-20 20:55:39 +0000

    Add 15.php

---
+++ src/15.php
@@ -0,0 +1,9 @@
+<?php
+// This is an example PHP script. It's not optimized or secure.
+// Do not use it in production.
+function displayError($message) {
+    echo "An error occurred: $message\n";
+}
+
+displayError("A simple error message.");
+?>
