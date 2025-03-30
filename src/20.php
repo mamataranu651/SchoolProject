7b2dadf399488fed5c57fb721b4e4c9d1eba124a
mamataranu651 <mamataranu651@gmail.com>
2025-03-30 12:02:35 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,13 @@
+<?php
+    // Example PHP code for generating a random password
+
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $length = 12;
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, strlen($chars) - 1)];
+    }
+
+    echo "Generated Password: " . $password;
+?>
